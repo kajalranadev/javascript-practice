ba7d2ee95fe866247cab0e27c86c6731975a5315
Kajal Rana <114815118+kajalranadev@users.noreply.github.com>
2023-11-24 15:17:42 +0530

    Create 21NOVB.js

---
+++ 21NOVB.js
@@ -0,0 +1,14 @@
+// Initialize the firstArray values [1, 2, 3, 4]
+let firstArray = [1, 2, 3, 4];
+
+// Assign the reference of firstArray to secondArray
+let secondArray = firstArray;
+
+// Modify the secondArray by adding 5 to the end
+secondArray.push(5);
+
+// firstArray to the console
+console.log(firstArray); // [1, 2, 3, 4, 5]
+
+// secondArray to the console
+console.log(secondArray); // [1, 2, 3, 4, 5]
